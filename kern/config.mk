a0430cfe4f6731fb82a495cb1567117877951668
Michael Sullivan <sully@msully.net>
2014-06-08 13:54:27 -0700

    Make the kern config.mk use $(CPP)

--- kern/config.mk
+++ kern/config.mk
@@ -1,6 +1,6 @@
 TABSTOP = 4
 
 %.c: %.mb
-	cpp -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ -P $< | ../compiler/mc --target c > $@
+	$(CPP) -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ -P $< | ../compiler/mc --target c > $@
 
 KERN_GAME_OBJS = kernel.o
