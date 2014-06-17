cbd122fdd8fa61fa6f21cf8f3819696603e6a41f
Amanda Watson <amanda@amwatson.org>
2014-06-16 23:59:55 -0700

    adding kernel code -- actual documentation in progress

--- kern/config.mk
+++ kern/config.mk
@@ -3,4 +3,4 @@ TABSTOP = 4
 %.c: %.mb
 	$(CPP) -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ -P $< | ../compiler/mc --target c > $@
 
-KERN_GAME_OBJS = kernel.o
+KERN_GAME_OBJS = kernel.o context/context.o
