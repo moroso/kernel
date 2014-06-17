26259dc31fa3bcc2e6ea01b1eaebc5887dbc639d
Michael Sullivan <sully@msully.net>
2014-06-17 00:31:02 -0700

    Make the makefile stuff work a bit better I hope.

--- kern/config.mk
+++ kern/config.mk
@@ -1,6 +1,10 @@
 TABSTOP = 4
 
 %.c: %.mb
-	$(CPP) -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ -P $< | ../compiler/mc --target c > $@
+# Need to delete the file if it doesn't work
+	$(CPP) -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ -P $< | ../compiler/mc --target c > $@ || (rm $@; false)
+
 
 KERN_GAME_OBJS = kernel.o context/context.o
+# Should generalize this better!
+STUKCLEANS += kern/kernel.c
