bc39b628628f54a90f6ced5fdc114e5f52977caf
Amanda Watson <amanda@amwatson.org>
2014-06-22 15:41:28 -0700

    I spent a day on a VM bug that turned out to be sully's fault

--- kern/config.mk
+++ kern/config.mk
@@ -4,7 +4,7 @@ TABSTOP = 4
 # Need to delete the file if it doesn't work
 	$(CPP) -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ $< | ../compiler/mc --target c > $@ || (rm $@; false)
 
-
+.SECONDARY: kern/kernel.c
 KERN_GAME_OBJS = kernel.o context/context.o
 # Should generalize this better!
 STUKCLEANS += kern/kernel.c
