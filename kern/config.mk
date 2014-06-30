cea9de4a8d2b888847d61dec5d634b414286ed66
Michael Sullivan <sully@msully.net>
2014-06-29 17:28:07 -0700

    Rename compiler binary to mbc.

--- kern/config.mk
+++ kern/config.mk
@@ -2,7 +2,7 @@ TABSTOP = 4
 
 %.c: %.mb
 # Need to delete the file if it doesn't work
-	$(CPP) -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ $< | ../compiler/mc --target c > $@ || (rm $@; false)
+	$(CPP) -MD -MP -MF $(@:.c=.$(DEP_SUFFIX)) -MT $@ $< | ../compiler/mbc --target c > $@ || (rm $@; false)
 
 .SECONDARY: kern/kernel.c
 KERN_GAME_OBJS = kernel.o context/context.o entry/entry_stubs_x86.o entry/entry_x86.o
