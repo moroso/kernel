0ece6d4e9fd1cf10b0b7bba65b90580491657f0b
Michael Sullivan <sully@msully.net>
2014-07-27 15:30:33 -0700

    Build the user file system object into the kernel binary.

--- kern/config.mk
+++ kern/config.mk
@@ -1,3 +1,6 @@
+# This really nasty config.mk slots into our temporary (?)
+# super hacky build system for testing the kernel on x86.
+
 TABSTOP = 4
 
 # Wee.
@@ -6,7 +9,22 @@ rwildcard=$(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2) $(filter $(subst 
 kern/kernel.c: $(call rwildcard,kern,*.mb)
 	$(CPP) kern/kernel.mb | ../compiler/mbc --target c -o $@
 
+# This is suuuuuper dubious.
+# The user directory sits next to the kern directory in the kernel/ repo,
+# but our bsenv setup just has a symlinm to kernel/kern.
+# However, kern/.. won't point to our current directory but will instead
+# point to the *actual* parent of the kern directory, which is the kernel/
+# root directory. kern/../user, then, points to kernel/user.
+# I didn't want the userspace build system to be built into the bsenv one,
+# though, so we use recursive make and I do a big glob to see if there is
+# anything worth trying to change down in there.
+# Welp.
+kern/fs_img.o: $(call rwildcard,kern/../user,*.mb *.c *.S *.h Makefile *.mk)
+	make -C kern/../user
+	cp kern/../user/build/fs_img.o $@
+
+
 .SECONDARY: kern/kernel.c
-KERN_GAME_OBJS = kernel.o context/context.o entry/entry_stubs_x86.o entry/entry_x86.o
+KERN_GAME_OBJS = kernel.o context/context.o entry/entry_stubs_x86.o entry/entry_x86.o fs_img.o
 # Should generalize this better!
 STUKCLEANS += kern/kernel.c
