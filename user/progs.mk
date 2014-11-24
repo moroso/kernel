fb3635b0424bcb27a63ffc5381670a060fe4602c
Amanda Watson <amanda@amwatson.org>
2014-11-23 20:53:32 -0500

    added tests, changed policy for async long messages (are ignored)

--- user/progs.mk
+++ user/progs.mk
@@ -1,3 +1,3 @@
 # Put a list of test programs to build here
 
-PROGS=basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout
+PROGS=basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout ipc_async_test
