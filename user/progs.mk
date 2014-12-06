b9455d3f9afe72aa087a2843d82bdfbd5a1b51de
Amanda Watson <amanda@amwatson.org>
2014-12-05 23:44:29 -0500

    added name server

--- user/progs.mk
+++ user/progs.mk
@@ -1,3 +1,3 @@
 # Put a list of test programs to build here
 
-PROGS= file_server_test #thrlib_test basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout ipc_async_test ipc_async_test2 ipc_manywait_test ipc_args_test
+PROGS= init file_server_test #thrlib_test basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout ipc_async_test ipc_async_test2 ipc_manywait_test ipc_args_test
