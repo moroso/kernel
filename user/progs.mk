15844b30a91b92a9cb042394c5974751bb2df25c
Amanda Watson <amanda@amwatson.org>
2014-12-08 02:03:52 -0500

    added mailbox test -- fixed some errors it found

--- user/progs.mk
+++ user/progs.mk
@@ -1,3 +1,3 @@
 # Put a list of test programs to build here
 
-PROGS= init file_server_test file_server name_server #thrlib_test basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout ipc_async_test ipc_async_test2 ipc_manywait_test ipc_args_test
+PROGS= init mailbox_test file_server_test file_server name_server #thrlib_test basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout ipc_async_test ipc_async_test2 ipc_manywait_test ipc_args_test
