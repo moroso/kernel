84e1a173898bc54dc2f4da62371164ead7b7efb6
Amanda Watson <amwatson@andrew.cmu.edu>
2014-12-15 11:24:42 -0800

    added in error handling, fixed return in ipclib

--- user/progs.mk
+++ user/progs.mk
@@ -1,3 +1,3 @@
 # Put a list of test programs to build here
 
-PROGS= init file_server name_server play_test mailbox_test mailbox_remove_test file_server_test  #thrlib_test basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout ipc_async_test ipc_async_test2 ipc_manywait_test ipc_args_test
+PROGS= init file_server name_server bad_server bad_test play_test mailbox_test mailbox_remove_test file_server_test  #thrlib_test basic_test basic_test2 repl_test repl_bide_test repl_perform_test child_test ipc_test ipc_timeout ipc_async_test ipc_async_test2 ipc_manywait_test ipc_args_test
